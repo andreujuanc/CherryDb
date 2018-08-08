dd97233c4e07b8010774f41cbcebf1246eda4564
Juan Carlos Andreu Gutierrez <andreujuanc@gmail.com>
2018-08-08 18:29:24 +0200

    added IStore

---
+++ client/src/data/IStore.ts
@@ -0,0 +1,15 @@
+import IRecord from "./IRecord";
+
+export default interface IStore {
+    
+    GetLastRecord(): IRecord ;
+    GetLastRecordTimeStamp(): number;
+    GetRecordById(id: string): IRecord;
+    GetAllRecords(): IRecord[];
+    
+    Upsert(records: IRecord | IRecord[]): IRecord[] | IRecord;
+    Count(): number;
+    
+    ClearPushData(records: IRecord | IRecord[]);
+    GetPushData(): Promise<IRecord | IRecord[]>;
+}
--- client/src/data/Store.ts
+++ client/src/data/Store.ts
@@ -1,6 +1,7 @@
 import IRecord from "./IRecord";
+import IStore from "./IStore";
 
-export default class Store {
+export default class Store  implements IStore{
     uuidv4() {
         return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
             var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
