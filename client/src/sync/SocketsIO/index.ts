b18408952d286b4cce8d21a1e7f51f9f261c680c
Juan Carlos Andreu <andreujuanc@gmail.com>
2019-02-19 11:54:20 +0100

    starting implementation

--- client/src/sync/SocketsIO/index.ts
+++ client/src/sync/SocketsIO/index.ts
@@ -3,6 +3,19 @@ import Remote from "../../endpoint/Remote";
 import ISync from '../ISync';
 
 export default class SocketsIOSync implements ISync {
+
+    //TODO: Considering having a base class for this
+    private _data: IStore;
+    private _remote: Remote;
+    private _started: Boolean;
+
+    public OnSyncCompleted: Function;
+
+    constructor(data: IStore, remote: Remote) {
+        this._data = data;
+        this._remote = remote;
+    }
+
     Start(): Promise<void> {
         return null;
     }    
@@ -10,7 +23,4 @@ export default class SocketsIOSync implements ISync {
     Stop(): Promise<void> {
         throw new Error("Method not implemented.");
     }
-
-    OnSyncCompleted: Function;
-
 }
